04d5bffe60ecc9697766ef890271a4f11429c694
panda8246 <2804937582@qq.com>
2025-07-09 17:30:52 +0800

    客户端ID

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,15 +1,49 @@
 import WebSocket from "ws";
+import readline from 'readline';
 
-const ws = new WebSocket('ws://localhost:52547');
+const socket = new WebSocket('ws://localhost:52547');
+let clientname = undefined;
 
-ws.on('open', () => {
+socket.on('open', () => {
     console.log('连接成功');
 });
 
-ws.on('message', (message) => {
-    console.log(`收到消息: ${message}`);
+socket.on('message', (message) => {
+    if (!clientname) {
+        clientname = message.toString();
+        createReadLine();
+    } else {
+        handleMsg(message.toString());
+        flush();
+    }
 });
 
-ws.on('close', () => {
+socket.on('close', () => {
     console.log('连接关闭');
 });
+
+
+function createReadLine() {
+    // 创建 readline 接口，用于从终端获取输入
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: `您的ID【${clientname}】请输入消息> `
+    });
+    rl.on('line', (input: string) => {
+        if (socket.readyState === WebSocket.OPEN) {
+            socket.send(input);
+        } else {
+            console.log('WebSocket 未连接，无法发送消息');
+        }
+    });
+}
+
+function handleMsg(msg: string) {
+    
+}
+
+function flush() {
+
+}
+
--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,21 +1,31 @@
+import { randomInt } from "crypto";
 import WebSocket from "ws";
 
 const server = new WebSocket.Server({ port: 52547 });
 const clients = new Map<string, WebSocket>();
 
+let nameList = ['牛', '马', '狗', '猴子', '广东双马尾', 'bro', '高斯林'];
+let idx = 1;
+
 server.on('connection', (socket, request) => {
     // request.headers.host 是客户端请求的 Host（即服务端的主机名和端口），不是客户端的 IP 和端口
     // 获取客户端的 IP 和端口可以通过 request.socket 获取
-    const clientName = request.socket.remoteAddress + ":" + request.socket.remotePort;
+    let clientName = nameList[randomInt(0, nameList.length - 1)];
+    if (clients.has(clientName)) {
+        clientName += `(${idx})`;
+    }
     console.log(`【${clientName}】连接成功`);
     clients.set(clientName, socket);
+    // 发送客户端名称
+    socket.send(clientName);
+    idx++;
     socket.on('message', (message) => {
         console.log(`【${clientName}】消息: ${message}`);
         clients.forEach((client) => {
             client.send(`【${clientName}】消息: ${message}`);
         });
     });
-        
+
     socket.on('close', () => {
         console.log(`【${clientName}】断开连接`);
         clients.delete(clientName);
